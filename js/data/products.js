aaaadd99f8918e20221a4f44f67fee938e0f26ee
Yana <yanaegorova69.3@gmail.com>
2021-09-30 15:58:06 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,161 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Cup Toys',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Colorful Baby Toys',
+                text: '8 brightly colored cups stack together with Unique Holes in the Bottom for water play. For babies 6 months+.',
+            },
+            {
+                title: 'Stacking Cups',
+                text: 'Children love to fit toy cups together, stack them up, or turn them over and hide things underneath.',
+            },
+            {
+                title: 'Baby Development Toys',
+                text: 'Large numbers on the bottom to practice counting & number recognition.',
+            },
+            {
+                text: 'Made without BPA, lead or phthalates.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png', './img/img1-5.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Dogs Christmas Clothes',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'The fabric is very soft and comfortable.',
+            },
+            {
+                text: 'Warm: dogs can wear it to withstand the cold winter.',
+            },
+            {
+                text: 'The back of this clothing has the elements of Santa Claus and antlers, perfect for Christmas wear, adding to the festive atmosphere.',
+            },
+            {
+                text: 'Size: M chest 15.7 inches, back length 11.8 inches, suitable for 5-7 pounds of dogs.',
+            },
+            {
+                text: 'Note: This is 2 legs, the size is mainly refers to the chest circumference. Measure the circumference of the dog\'s chest width, the measured circumference set aside 2-3 cm for the actual chest length.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Round Tray',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Decorative Tray',
+                text: 'Decorative circular-shaped contemporary tray with polished metal handles will be an elegant accent in your home.',
+            },
+            {
+                title: 'For Display Or Storage',
+                text: 'Tray is perfect for use on entryway table, vanity, ottoman, or even for serving drinks!',
+            },
+            {
+                title: 'Large Serving Tray',
+                text: 'Plenty of surface area with an 15.5-inch diameter and a 1.75-inch depth.',
+            },
+            {
+                title: 'Clean Modern Storage',
+                text: 'Modern design-inspired round tray requires no assembly and is ready to use as a decorative tray in your home.',
+            },
+            {
+                title: 'Decorative Glossy Tray',
+                text: 'Decorative tray is constructed of sturdy MDF with glossy finish and polished metal handles.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Hammer Handpiece',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Hammer Handpiece for Eurotool Flex Shaft System.',
+            },
+            {
+                text: 'Adjustable Impact.',
+            },
+            {
+                text: '1/32',
+            },
+            {
+                text: 'Up to 5,000 RPM.',
+            },
+            {
+                text: 'Includes Replacement Anvil Point.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Convertible Toddler Bed',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Convenient Transition Bed',
+                text: '3-in-1 Convertible Toddler Bed sports a low-to-floor design making it easy for your child to get in and out of bed. The bed comes with 4 wooden safety side rails and is reinforced for stability with center legs.',
+            },
+            {
+                title: '3 in 1 Convertibility',
+                text: 'Converts easily from a toddler bed into 2 kid-sized chairs and a table that complement any room setting. Keeps pace with the changing needs of your growing child. Conversion kit sold separately.',
+            },
+            {
+                title: 'Beautiful Aesthetics',
+                text: 'Whether you want a traditional toddler room or a modern one, the elegant fits in easily with its chic bell-shaped full panel headboard and footboard. Built of New Zealand pinewood, it is available in 5 elegant finishes.',
+            },
+            {
+                title: 'Safety Focus',
+                text: 'All our products are designed with the safety of your children in mind. The Portland is JPMA certified & meets/exceeds all applicable ASTM and CPSC standards. It is free of phthalates, latex, lead and BPA.',
+            },
+            {
+                title: 'Products Specs',
+                text: 'The bed dimensions are 57Lx29Wx28H inches. Each chair measures 29Lx29Wx18H; The table is 29Lx14Wx8H. Max weight limit for the bed is 50 lbs.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Liquid Cooler',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Design',
+                text: 'With a re-designed cap and larger infinity mirror ring LED, allows for adjusting the orientation of the pump head to fit without affecting the direction of the logo.',
+            },
+            {
+                title: 'Bigger And Brighter',
+                text: '10% bigger infinitty mirror cap allows for more vivid RGB and a rotating cap accommodates re-orienting the logo no matter the direction the cooler is installed.',
+            },
+            {
+                title: 'Better Control',
+                text: 'Full CAM integration allows you to manage your performance with precision.',
+            },
+            {
+                title: 'Incredible Performance',
+                text: 'The Aer P radiator fans feature a chamfered intake and fluid dynamic bearing that provides silent operations, durability, and powerful cooling performance.',
+            },
+            {
+                title: 'Simple Installation',
+                text: 'Fine nylon mesh sleeves strengthen the rubber tubing, providing durability and protection against mishandling.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png'],
+    },
+]
